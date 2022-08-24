1b8b65ff549783549946f0ba19cb1a01472a3037
Shridhar s <Shridharsirpee15@gmail.com>
2022-08-24 14:51:27 +0530

    Added Class

---
+++ src/test/java/com/sdet38/samplemvnproject/Dummy1.java
@@ -0,0 +1,10 @@
+package com.sdet38.samplemvnproject;
+
+public class Dummy1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello its me");
+	}
+
+}
--- src/test/java/com/sdet38/samplemvnproject/sampleclass.java
+++ src/test/java/com/sdet38/samplemvnproject/sampleclass.java
@@ -3,7 +3,7 @@ package com.sdet38.samplemvnproject;
 public class sampleclass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
     System.out.println("hello");
 	}
 
